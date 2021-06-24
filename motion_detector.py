b29d4cf579b77593fe844c0511be22170ac3373c
Ahmad Zafar <62062766+TheTurtle3@users.noreply.github.com>
2021-06-23 20:55:40 -0400

    Timestamps of motion detected are now viewable with Graph.html

--- motion_detector.py
+++ motion_detector.py
@@ -31,6 +31,7 @@ while True:
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
     status_list.append(status)
+    status_list = status_list[-2:]
 
     if status_list[-1] == 1 and status_list[-2] == 0:
         times.append(datetime.now())
